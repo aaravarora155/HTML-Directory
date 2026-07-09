47a8b2fea8e07f00ed9ce92bb41d0d6fc71d447b
aaravarora155 <aarav2607@gmail.com>
2026-07-09 12:22:36 +0100

    Fixed Loading Issues for Manual Interpreter

--- FRC Game Manual Interpreter/server.js
+++ FRC Game Manual Interpreter/server.js
@@ -31,9 +31,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`\n🚀 FRC Manual Interpreter UI running at http://localhost:${PORT}`);
-  console.log(`   Proxying /api/query → ${FLASK_URL}/api/query\n`);
-});
-
 export default app;
